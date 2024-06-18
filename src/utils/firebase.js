4d6cc210bd1d29176fc0ab86ac024947a9193b44
Mary Kilduff <marykilduff@Marys-MacBook-Pro-3.local>
2024-06-18 10:15:56 +0100

    intial commit

---
+++ src/utils/firebase.js
@@ -0,0 +1,21 @@
+import { initializeApp } from "firebase";
+import { fireStore, getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDZGNCAffFBs-fCoAFpVVI8HbsWMxRFKFI",
+  authDomain: "gamesource-64f12.firebaseapp.com",
+  projectId: "gamesource-64f12",
+  storageBucket: "gamesource-64f12.appspot.com",
+  messagingSenderId: "915108996912",
+  appId: "1:915108996912:web:052ff48c76d50d1f1ccaf5",
+  measurementId: "G-01K4TV1F7E",
+};
+
+initializeApp(firebaseConfig);
+
+const DB = getFirestore();
+const AUTH = getAuth();
+
+export { DB, AUTH };
