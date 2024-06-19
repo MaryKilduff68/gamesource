1e834ca9ce046ea0215982cf1ae634d58e78642b
Mary Kilduff <marykilduff@Marys-MacBook-Pro-3.local>
2024-06-19 13:36:28 +0100

    signout

--- src/stores/user.js
+++ src/stores/user.js
@@ -4,108 +4,115 @@ import router from "@/router";
 /// FIREBASE
 import { AUTH, DB } from "@/utils/firebase";
 import {
-  createUserWithEmailAndPassword,
-  signInWithEmailAndPassword,
-  signOut,
+	createUserWithEmailAndPassword,
+	signInWithEmailAndPassword,
+	signOut,
 } from "firebase/auth";
 import { getDoc, doc, setDoc, updateDoc } from "firebase/firestore";
 import errorCodes from "@/utils/fbcodes";
 
 const DEFAULT_USER = {
-  uid: null,
-  email: null,
-  firstname: null,
-  lastname: null,
-  isAdmin: null,
+	uid: null,
+	email: null,
+	firstname: null,
+	lastname: null,
+	isAdmin: null,
 };
 
 export const useUserStore = defineStore("user", {
-  state: () => ({
-    loading: false,
-    user: DEFAULT_USER,
-    auth: false,
-  }),
-  getters: {},
-  actions: {
-    setUser(user) {
-      this.user = { ...this.user, ...user };
-      this.auth = true;
-    },
-    async autoSignIn(uid) {
-      try {
-        const userData = await this.getUserProfile(uid);
+	state: () => ({
+		loading: false,
+		user: DEFAULT_USER,
+		auth: false,
+	}),
+	getters: {},
+	actions: {
+		setUser(user) {
+			this.user = { ...this.user, ...user };
+			this.auth = true;
+		},
+		async autoSignIn(uid) {
+			try {
+				const userData = await this.getUserProfile(uid);
 
-        // Update local state
-        this.setUser(userData);
-        return true;
-      } catch (error) {
-        console.log(error);
-      }
-    },
-    async getUserProfile(uid) {
-      try {
-        const userRef = await getDoc(doc(DB, "users", uid));
-        if (!userRef.exists()) {
-          throw new Error("Could not find user !!");
-        }
-        return userRef.data();
-      } catch (error) {
-        throw new Error(error);
-      }
-    },
-    async signIn(formData) {
-      try {
-        this.loading = true;
+				// Update local state
+				this.setUser(userData);
+				return true;
+			} catch (error) {
+				console.log(error);
+			}
+		},
+		async getUserProfile(uid) {
+			try {
+				const userRef = await getDoc(doc(DB, "users", uid));
+				if (!userRef.exists()) {
+					throw new Error("Could not find user !!");
+				}
+				return userRef.data();
+			} catch (error) {
+				throw new Error(error);
+			}
+		},
+		async signIn(formData) {
+			try {
+				this.loading = true;
 
-        /// SIGN IN USER
-        const response = await signInWithEmailAndPassword(
-          AUTH,
-          formData.email,
-          formData.password
-        );
-        /// GET USER DATA
-        const userData = await this.getUserProfile(response.user.uid);
+				/// SIGN IN USER
+				const response = await signInWithEmailAndPassword(
+					AUTH,
+					formData.email,
+					formData.password
+				);
+				/// GET USER DATA
+				const userData = await this.getUserProfile(response.user.uid);
 
-        /// UPDATE LOCAL STATE
-        this.setUser(userData);
+				/// UPDATE LOCAL STATE
+				this.setUser(userData);
 
-        // REDIRECT USER
-        router.push({ name: "dashboard" });
-      } catch (error) {
-        throw new Error(errorCodes(error.code));
-      } finally {
-        this.loading = false;
-      }
-    },
-    async register(formData) {
-      try {
-        this.loading = true;
+				// REDIRECT USER
+				router.push({ name: "dashboard" });
+			} catch (error) {
+				throw new Error(errorCodes(error.code));
+			} finally {
+				this.loading = false;
+			}
+		},
 
-        /// REGISTER USER
-        const response = await createUserWithEmailAndPassword(
-          AUTH,
-          formData.email,
-          formData.password
-        );
+		async signOut() {
+			await signOut(AUTH);
+			this.user = DEFAULT_USER;
+			this.auth = false;
+			router.push({ name: "home" });
+		},
+		async register(formData) {
+			try {
+				this.loading = true;
 
-        // ADD USER TO DB
-        const newUser = {
-          uid: response.user.uid,
-          email: response.user.email,
-          isAdmin: false,
-        };
-        await setDoc(doc(DB, "users", response.user.uid), newUser);
+				/// REGISTER USER
+				const response = await createUserWithEmailAndPassword(
+					AUTH,
+					formData.email,
+					formData.password
+				);
 
-        /// UPDATE LOCAL STATE
-        this.setUser(newUser);
+				// ADD USER TO DB
+				const newUser = {
+					uid: response.user.uid,
+					email: response.user.email,
+					isAdmin: false,
+				};
+				await setDoc(doc(DB, "users", response.user.uid), newUser);
 
-        // REDIRECT USER
-        router.push({ name: "dashboard" });
-      } catch (error) {
-        throw new Error(errorCodes(error.code));
-      } finally {
-        this.loading = false;
-      }
-    },
-  },
+				/// UPDATE LOCAL STATE
+				this.setUser(newUser);
+
+				// REDIRECT USER
+				router.push({ name: "dashboard" });
+			} catch (error) {
+				throw new Error(errorCodes(error.code));
+			} finally {
+				this.loading = false;
+			}
+		},
+	},
 });
